790c4194c038eff52014a2efe246e5dc9f767f0e
lao sj <shengshengdasha@foxmail.com>
2023-06-04 12:39:42 +0800

    添加

--- video.py
+++ video.py
@@ -1,34 +1,53 @@
 import requests
 import os
 from bs4 import BeautifulSoup
-import moviepy.editor as mp
 
+import re
 import random
 
-url="https://29maoek.com/vodtypehtml/9_2.html"
+url="https://29maoek.com/vodtypehtml/9_2.html"#链接
+
 header={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) App\
         leWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg\
         /113.0.1774.57"}
-html_vidio=[]
+html_vidio=[]#储存一级链接
 
+#请求一级链接
 response=requests.get(url,headers=header)
 text=response.text
 html=BeautifulSoup(text,"html.parser")
 
-html_url=html.find_all("a",class_="videoinfo")
 
+html_url=html.find_all("a",class_="videoinfo")
 for i in html_url:
     html_vidio.append(i['href'])
 
 
-vidio_url="https://29maoek.com/"+html_vidio[8]
+# vidio_url="https://29maoek.com/"+html_vidio[8]
+vidio_url="https://29maoek.com/maoplay/254442_1_1.html"
 response=requests.get(vidio_url,headers=header)
-text=response.text
-html=BeautifulSoup(text,"html.parser")
 
-video_html=html.find("video")
 
-video_url=video_html.get("src")
+
+pattern = r'var player_aaaa=(.*?);'
+match = re.search(pattern, response.text)
+if match:
+    player_aaaa_value = match.group(1)
+    print(player_aaaa_value)
+else:
+    print("No match found.")
+
+
+
+# a=re.search(r'var player_aaaa=(.*?);',response.text)
+# print(a)
+
+
+# print(response.text)
+
+
+
+
 
 
 
